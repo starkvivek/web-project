2ead138f8af2bbf9b73f5a11062b5024776ff9fe
Narra Vivek <153751863+starkvivek@users.noreply.github.com>
2025-06-13 18:30:04 +0530

    Add files via upload

---
+++ StayFinder_Starter/client/README.md
@@ -0,0 +1,1 @@
+# StayFinder Frontend (React)
---
+++ StayFinder_Starter/server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = express();
+dotenv.config();
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}).catch(err => console.error(err));
